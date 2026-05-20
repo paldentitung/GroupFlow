035fcfbdd2104d38458e1c3cc8a16313b92e45f0
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-20 22:10:08 +0545

    Feat: add db connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+dotenv.config({
+  path: "./.env.development",
+});
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+// database connection
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
