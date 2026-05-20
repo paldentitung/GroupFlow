e20b66d8ec2aa9fe49aada6a45d58b668c247cb8
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-20 09:12:47 +0545

    feat: add setup basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
