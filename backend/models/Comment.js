494b0d11159defd78e180e6e5acd5cfe06148d23
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-27 18:17:00 +0545

    Feat: add comment schema

---
+++ backend/models/Comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
