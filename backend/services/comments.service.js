590b2068d3b75bc7dca4aa81b74508c3fb3dc217
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-28 20:29:04 +0545

    refactor: the comment logic to service

---
+++ backend/services/comments.service.js
@@ -0,0 +1,63 @@
+import Task from "../models/Task.js";
+import Comment from "../models/Comment.js";
+import AppError from "../utils/AppError.js";
+export const getCommentsService = async (taskId) => {
+  const task = await Task.findById(taskId);
+
+  if (!task) {
+    throw new AppError("Task not found", 404);
+  }
+
+  const comments = await Comment.find({ taskId })
+    .populate("authorId", "firstName lastName avatar")
+    .sort({ createdAt: -1 });
+
+  return await comment.populate("authorId", "firstName lastName avatar");
+};
+
+export const createCommentService = async (taskId, content, userId) => {
+  const task = await Task.findById(taskId);
+
+  if (!task) {
+    throw new AppError("Task not found", 404);
+  }
+
+  const comment = await Comment.create({
+    content,
+    authorId: userId,
+    taskId,
+  });
+
+  return comment;
+};
+
+export const updateCommentService = async (commentId, content, userId) => {
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new AppError("Comment not found", 404);
+  }
+
+  if (!comment.authorId.equals(userId)) {
+    throw new AppError("You can only edit your own comments", 403);
+  }
+
+  comment.content = content;
+  await comment.save();
+
+  return comment;
+};
+
+export const deleteCommentService = async (commentId, userId) => {
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new AppError("Comment not found", 404);
+  }
+
+  if (!comment.authorId.equals(userId)) {
+    throw new AppError("You can only delete your own comments", 403);
+  }
+
+  await comment.deleteOne();
+};
