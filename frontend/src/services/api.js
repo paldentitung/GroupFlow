676d93a66146e7fdddca14ffc9dcf37523f7f280
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-28 21:42:19 +0545

    feat: add api layer

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_BASE_URL = import.meta.env.API_URL;
+
+const request = async (url, options, auth = true) => {
+  const res = await fetch(`${API_BASE_URL}${url}`, {
+    ...(auth && { credentials: "include" }),
+    ...options,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+
+  return data;
+};
+
+export default request;
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,37 @@
+import { request } from "./api";
+
+export const register = async (data) => {
+  return request(
+    "/auth/register",
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    },
+    false,
+  );
+};
+
+export const login = async (data) => {
+  return request(
+    "/auth/login",
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    },
+    false,
+  );
+};
+
+export const verifyEmail = async (token) => {
+  return request(
+    "/auth/verify-email",
+    {
+      method: "POST",
+      body: JSON.stringify({ token }),
+      headers: { "Content-Type": "application/json" },
+    },
+    false,
+  );
+};
