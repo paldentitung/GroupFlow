8efe531aba46a5e1fe7458baeda13563fcc1483e
Palden D. Titung <paldendorjetitung@gmail.com>
2026-06-03 17:20:52 +0545

    Feat: add notification schema

---
+++ backend/models/Notification.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
+    type: {
+      type: String,
+      enum: [
+        "task_assigned",
+        "task_completed",
+        "comment_added",
+        "member_invited",
+        "project_updated",
+      ],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String, // "/projects/:id/tasks/:id"
+      default: null,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
