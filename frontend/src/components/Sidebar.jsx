9bbb80aa93fe6656f60864264fe1305c7210d7ad
Palden D. Titung <paldendorjetitung@gmail.com>
2026-06-07 23:52:57 +0545

    fix: add different icon for my task and task page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -23,13 +23,13 @@ const navItems = [
       },
       {
         name: "My Tasks",
-        icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4",
+        icon: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z",
         badge: 12,
         link: "/my-tasks",
       },
       {
         name: "Tasks",
-        icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4",
+        icon: "M4 6h16M4 10h16M4 14h10",
         badge: 12,
         link: "/tasks",
       },
@@ -165,7 +165,6 @@ export default function Sidebar() {
                 <span className="text-sm font-medium text-gray-800 truncate">
                   {user?.firstName} {user?.lastName}
                 </span>
-                s
                 <span className="text-xs text-gray-500 truncate">
                   {user?.email}
                 </span>
