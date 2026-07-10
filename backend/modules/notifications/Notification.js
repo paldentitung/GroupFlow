7eb71509d45ca5f4a50de7798c232425bb03e726
Palden D. Titung <paldendorjetitung@gmail.com>
2026-07-10 23:29:38 +0545

    feat(notification): auto delete notifications after 30 days

--- backend/modules/notifications/Notification.js
+++ backend/modules/notifications/Notification.js
@@ -38,6 +38,11 @@ const notificationSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    expiresAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+      expires: 0,
+    },
   },
   { timestamps: true },
 );
